e30379c83633b45b99dd6c5bf85251b6e7b24e9e
Paige Wilson <hello.pmwilson@gmail.com>
2024-01-01 11:08:00 -0500

    removed extraneous lines

--- cell_bio_lab/inventory/static/inventory/js/inventory.js
+++ cell_bio_lab/inventory/static/inventory/js/inventory.js
@@ -1,5 +1,3 @@
-console.log("hello change 2");
-
 document.getElementById('requestView').addEventListener('click', function(e){
     e.preventDefault();
     let url = e.target.href;
@@ -7,23 +5,14 @@ document.getElementById('requestView').addEventListener('click', function(e){
 
     loadContent(url);
     console.log('after');
-    
-    // async function loadView(url) {
-    //     console.log('this step happened');
-    //     try {
-    //         console.log('try');
-    //         const loadRequest = await fetch(url);
-    //         if (!loadRequest.ok) {
-    //             throw new Error('Error! Status: ${loadRequest.status}');
-    //         }
-    //     console.log(loadRequest);
-    //     } catch (error) {
-    //         console.error('Error:', error);
-    //     }
-    // };
 });
 
 function loadContent(url) {
-    console.log('ajax');
     $('#content').load(url);
 }
+
+$('#tabs').click(function() {
+    console.log(click);
+})
+
+console.log("bitches");
